095066e51d90f3d43dd1529d4f1e2c402306cc0c
oltionchampari <oltion.champari@datavisyn.io>
2023-02-27 15:55:28 +0100

    Migration to visyn_core

--- src/pages/components/SourceCodeCard.tsx
+++ src/pages/components/SourceCodeCard.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import {AppMetaDataUtils, useAsync} from 'tdp_core';
+import { useAsync } from 'visyn_core';
+import { AppMetaDataUtils } from 'tdp_core';
 
 export function SourceCodeCard() {
   const loadMetaData = React.useMemo(() => () => AppMetaDataUtils.getMetaData(), []);
-  const {status, value} = useAsync(loadMetaData, []);
+  const { status, value } = useAsync(loadMetaData, []);
 
   return (
     <>
       <div className="card shadow-sm p-2">
         <div className="card-body">
           <p className="card-text">
-            The source code of Coral is released at <a href="https://github.com/Caleydo/Coral" target="_blank" rel="noopener">GitHub</a>.
+            The source code of Coral is released at{' '}
+            <a href="https://github.com/Caleydo/Coral" target="_blank" rel="noopener">
+              GitHub
+            </a>
+            .
           </p>
           <p className="card-text">
-            This application is part of Phovea, a platform for developing
-            web-based visualization applications. For tutorials, API docs, and
-            more information about the build and deployment process, see the
-            documentation page.
+            This application is part of Phovea, a platform for developing web-based visualization applications. For tutorials, API docs, and more information
+            about the build and deployment process, see the documentation page.
           </p>
-          <p className="card-text"><b>Version: </b> {(status === 'success') ? value.version : 'Fetching current version ...'}</p>
+          <p className="card-text">
+            <b>Version: </b> {status === 'success' ? value.version : 'Fetching current version ...'}
+          </p>
         </div>
       </div>
     </>
